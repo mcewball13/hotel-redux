e60ff9ed1bca3381bf7db0d164996fc5be6e8159
Frostquill <ruiz82494@gmail.com>
2021-09-16 14:40:03 -0700

    resolvers starte

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,39 @@
 const { Employee, Guest, Room } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
     rooms: async () => {
-      return Room.find();
+      return Room.find()
+      .populate('Guest.schema')
+
     },
-    // book: async (parent, { title }) => {
-    //   return Book.findOne({ title });
-    // }
-  },
-  // Mutation: {
-  //   addBook: async (parent, args) => {
-  //     const book = await Book.create(args);
-  //     return book;
-  //   }
-  // }
-};
+    room: async (parent, args, context) => {
+        if(context.room) {
+          const roomData = await Room.findOne({_id: context.room.room_id,
+          })
+          .populate('Guest.schema');
+
+          return roomData;
+        }
+        throw new AuthenticationError('Not logged in');
+      },
+      guest: async () => {
+        if(context.guest) {
+          const guestData = await Guest.findOne({name: context.guest.name})
+
+          return guestData
+        }
+      },
+    },
+  Mutation: {
+    addUser: async () => {}
+      
+    }
+
+
+  
+  }
+
 
 module.exports = resolvers;
