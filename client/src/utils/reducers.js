b5c3a21d447454d189f368d15f5951e58aa9534a
mcewball13 <mcewen1984@hotmail.com>
2021-09-19 15:43:32 -0700

    added header pump functionality with global state

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,26 +1,23 @@
-import {
-    ACTIVE_LOGIN_PLATE,
-    HEADER_ACTIVE
-} from "./actions"
-import { useReducer } from 'react';
+import { ACTIVE_LOGIN_PLATE, HEADER_ACTIVE } from "./actions";
+import { useReducer } from "react";
 
 export const reducer = (state, action) => {
     switch (action.type) {
         case ACTIVE_LOGIN_PLATE:
             return {
                 ...state,
-              isLoginPlate: !state.isLoginPlate,
-            }
-            case HEADER_ACTIVE:
-                return {
-                    ...state,
-                    isActive: !state.isActive,
-                }
-            
+                isLoginPlate: !state.isLoginPlate,
+            };
+        case HEADER_ACTIVE:
+            return {
+                ...state,
+                isActive: !state.isActive,
+            };
+
         default:
             return state;
     }
-}
+};
 export function useUiReducer(initialState) {
     return useReducer(reducer, initialState);
-  }
+}
