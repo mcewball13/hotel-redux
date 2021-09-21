ab848ebcf535634dd25f5f7e53d6d1b791d7fe9c
Frostquill <ruiz82494@gmail.com>
2021-09-21 13:39:56 -0700

    login function complete

--- client/src/components/LoginPageComp/LoginForm/index.js
+++ client/src/components/LoginPageComp/LoginForm/index.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import { useStoreContext } from "../../../utils/GlobalState";
 import { ACTIVE_LOGIN_PLATE } from "../../../utils/actions";
+import { LOGIN } from '../../../utils/mutations';
+import Auth from '../../../utils/auth';
+import { useMutation } from "@apollo/client";
 
 const LoginForm = () => {
+    const [formState, setFormState] = useState({email: '', password: '' });
+    const [login, {error}] = useMutation(LOGIN);
     const [_, dispatch] = useStoreContext();
 
     const handlePlateChange = () => {
         dispatch({
             type: ACTIVE_LOGIN_PLATE,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const {data} = await login({
+                variables: { email: formState.email, password: formState.password},
+            });
+            
+            Auth.login(data.login.token);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
         });
     };
 
@@ -38,23 +65,33 @@ const LoginForm = () => {
                 <Grid item xs={12} md={8}>
                     <TextField
                         id="username"
-                        label="Username"
+                        label="Email"
+                        name="email"
                         variant="standard"
                         sx={{
                             width: "100%",
                         }}
+                        onChange={handleChange}
                     />
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <TextField
                         id="password"
                         label="Password"
+                        name="password"
                         variant="standard"
                         sx={{
                             width: "100%",
                         }}
+                        onChange={handleChange}
+
                     />
                 </Grid>
+                {error ? (
+                    <div> 
+                        <p className = "error-text">The provided password is Incorrect</p>
+                    </div>
+                ) : null}
                 <Grid item xs={12} md={8}>
                     <Button
                         color="success"
@@ -62,6 +99,7 @@ const LoginForm = () => {
                         sx={{
                             width: "100%",
                         }}
+                        onClick={handleFormSubmit}
                     >
                         Login
                     </Button>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -6,6 +6,7 @@ export const LOGIN = gql`
       token
       employee {
         _id
+        
       }
     }
   }
